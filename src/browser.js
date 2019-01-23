3d33aed2cd3a4ecca0ddcb08808348c87050b29a
许智源 <xuzhiyuan@souche.com>
2019-01-24 00:31:32 +0800

    [feat] 样式修改

--- src/browser.js
+++ src/browser.js
@@ -2,25 +2,40 @@ const elementReady = require('element-ready');
 const fs = require('fs');
 const path = require('path');
 
+const hiddenLoading = () => {
+  const loading = document.getElementById('loadingWrap');
+  document.body.removeChild(loading);
+};
+
 const addStyle = () => {
   const fileName = path.resolve(__dirname, 'style/index.css');
   fs.readFile(fileName, (err, data) => {
     const style = document.createElement('style');
     style.innerHTML = data.toString().trim().replace(/\n/g, '');
     document.head.appendChild(style);
+    setTimeout(() => {
+      hiddenLoading();
+    }, 1000);
   });
 };
 
 
-function addDraggable() {
+const addDraggable = () => {
   const div = document.createElement('div');
   div.className = 'draggable';
-  div.style = 'position: fixed;left: 0px;width: 25px;z-index: 9999;top: 0px;height: 25px;background: #000;';
+  document.querySelector('nav').appendChild(div);
+};
+
+const showLoading = () => {
+  const div = document.createElement('div');
+  div.className = 'loadingWrap';
+  div.id = 'loadingWrap';
   document.body.appendChild(div);
-}
+};
 
 
 document.addEventListener('DOMContentLoaded', () => {
+  showLoading();
   elementReady('#rootFooter').then(() => {
     addStyle();
     addDraggable();
